222f49ce3ef004f29b554ec85e2663827e349ddf
justinsprecco <justin@sprecco.com>
2024-03-24 10:33:04 -0700

    modify Error to class-based approach

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -1,12 +1,16 @@
+import { Component } from "react";
 import PropTypes from "prop-types";
 
-function ErrorMessage({ error }) {
-  return (
-    <div className="error-div">
-      <strong className="font-bold">Error: </strong>
-      <span className="block">{error}</span>
-    </div>
-  );
+class ErrorMessage extends Component {
+  render() {
+    const { error } = this.props;
+    return (
+      <div className="error-div">
+        <strong className="font-bold">Error: </strong>
+        <span className="block">{error}</span>
+      </div>
+    );
+  }
 }
 
 ErrorMessage.propTypes = {
