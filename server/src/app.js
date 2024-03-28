d94c2f1c034dd19a5786bd40b13eee3d5f38aa81
justinsprecco <justin@sprecco.com>
2024-03-28 01:07:15 -0700

    absolute paths

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 import { responseHandler } from "./middleware/response.handler.js";
 import authRoutes from "./routes/auth.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 
-const app = express();
+// Absolute Paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Reverse-Proxy
-app.set("trust proxy", 1);
+// Express Setup
+const app = express();
 
 // Middleware
 app.use(cors());
@@ -21,6 +25,11 @@ app.use("/api/project", projectRoutes);
 app.use("/api/task", taskRoutes);
 
 // Static files
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Catch Route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 export default app;
